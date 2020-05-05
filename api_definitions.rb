fa26f6c2170a5eee3868fb417e70be20be269f05
miramanz <miraclemanzongo@gmail.com>
2020-05-05 11:27:01 +0200

    - refactor methods to get and post api request

--- api_definitions.rb
+++ api_definitions.rb
@@ -8,35 +8,39 @@ class ApiDefinitions
     'The Modern Ruby API wrapper is running'
   end
 
-  def setup_http(url)
+  def run_api_request_get(url, use_ssl, headers = {})
+    uri, http       = setup_http(url, use_ssl)
+    api_request     = Net::HTTP::Get.new(uri.request_uri)
+    headers.each do |key, val|
+      api_request.add_field(key.to_s, val.to_s)
+    end
+
+    response = http.request(api_request)
+    [response.code, response.body]
+  end
+
+  def run_api_request_post(url, use_ssl, params, headers = {})
+    uri, http       = setup_http(url, use_ssl)
+    api_request     = Net::HTTP::Post.new(uri.request_uri)
+    headers.each do |key, val|
+      api_request.add_field(key.to_s, val.to_s)
+    end
+
+    api_request.set_form_data(params)
+    response = http.request(api_request)
+    [response.code, response.body]
+  end
+
+  private
+
+  def setup_http(url, use_ssl)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
 
     http.open_timeout = 2
     http.read_timeout = 600
-    http.use_ssl = true
+    http.use_ssl = true if use_ssl
 
     [uri, http]
-  end
-
-  def run_api_request_get(url, request_fields = {})
-    uri, http       = setup_http(url)
-    api_request     = Net::HTTP::Get.new(uri.request_uri)
-    request_fields.each do |key, val|
-      api_request.add_field(key.to_s, val.to_s)
-    end
-
-    http.request(api_request)
-  end
-
-  def run_api_request_post(url, params, request_fields = {})
-    uri, http       = setup_http(url)
-    api_request     = Net::HTTP::Post.new(uri.request_uri)
-    request_fields.each do |key, val|
-      api_request.add_field(key.to_s, val.to_s)
-    end
-
-    api_request.set_form_data(params)
-    http.request(api_request)
   end
 end
